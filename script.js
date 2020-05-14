4ca8a8bf44a430a8170151fa9c3c975d90bfb743
firmament12 <65328325+firmament12@users.noreply.github.com>
2020-05-14 14:23:26 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,81 @@
+$(function(){
+// アニメ
+//  $(window).scrollTop(0);
+//    $(".mainSite").css("display", "none");
+//    $(".mainSite").css({opacity:'0'});
+//    setTimeout(function(){
+//      $(".mainSite").css("display", "block");
+//      $(".mainSite").stop().animate({opacity:'1'},400);
+//    },3000);
+//    setTimeout(function(){
+//      $(".anime").slideUp(1000);
+//    },2000);
+  
+// スムーススクロール
+  $('a[href^="#"]').not('.design>li>a').on('touchend click',function(smooth) {
+    smooth.preventDefault();
+    var href= $(this).attr('href');
+    var target = $(href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, 300, 'swing');
+    return false;
+  });
+  
+// タブパネル
+  $('#tab1>li>a').on('touchend click',function(){
+    $('*').removeClass('active2');
+    $(this).parent('li').addClass('active1').siblings('li').removeClass('active1');
+    $('#tab2>li>a').css('border-right','1px solid #ccc');
+    var target = $(this).attr('href');
+    $(target).addClass('active1').siblings('.panel').removeClass('active1');
+    return false;
+  });
+  $('#tab2>li>a').on('touchend click',function(){
+    $('*').removeClass('active1');
+    $(this).parent('li').addClass('active2').siblings('li').removeClass('active2');
+    $('#tab2>li>a').css('border-right','1px solid #fff');
+    var target = $(this).attr('href');
+    $(target).addClass('active2').siblings('.panel').removeClass('active2');
+    return false;
+  });
+  
+// サイコロ
+  document.getElementById('sai').insertAdjacentHTML('afterbegin',
+   '<img src="img/dice/dice1.png" alt="dice1"><img src="img/dice/dice2.png" alt="dice2"><img src="img/dice/dice3.png" alt="dice3"><img src="img/dice/dice4.png" alt="dice4"><img src="img/dice/dice5.png" alt="dice5"><img src="img/dice/dice6.png" alt="dice6">');
+  var dice =
+   ['<img src="img/dice/dice1.png" alt="dice1">',
+    '<img src="img/dice/dice2.png" alt="dice2">',
+    '<img src="img/dice/dice3.png" alt="dice3">',
+    '<img src="img/dice/dice4.png" alt="dice4">',
+    '<img src="img/dice/dice5.png" alt="dice5">',
+    '<img src="img/dice/dice6.png" alt="dice6">'];
+  $('#dice').on('touchend click',function(event){
+     event.preventDefault();
+  var rnd = Math.floor(Math.random()*dice.length);
+     var shuffle = dice[rnd];
+    if (dice.length === 6) {
+      switch (rnd) {
+          case 0:
+            rnd++;
+            shuffle = dice[rnd];
+            $('#dice').html(shuffle);
+          break;
+          default:
+            $('#dice').html(shuffle);
+          break;
+      }
+          dice.splice(rnd,1);
+          localStorage['add'] = shuffle;
+    } else {
+          $('#dice').html(shuffle);
+          dice.splice(rnd,1);
+          dice.splice(4,0,localStorage['add']);
+          localStorage['add'] = shuffle;
+      }
+  });
+  
+// コードを表示
+  $('#list').load('list.txt');
+  
+  
+});
